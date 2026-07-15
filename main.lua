3e75068b031fc26d6e4082f2f515cb5d2f16eb27
tyxca01 <290634522+tyxca01@users.noreply.github.com>
2026-07-15 22:16:10 +0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,8 @@
 local placeIdStr = string.format("%.0f", game.PlaceId)
+local place = game.PlaceId
 if placeIdStr == "2753915549" or placeIdStr == "79091703265657" or placeIdStr == "100117331123089" then 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/tyxca01/ActriumHub/refs/heads/main/BloxFruits.lua"))()
-elseif placeIdStr == "97598239454123" or placeIdStr == "77085202503540" then 
+elseif place == "97598239454123" or place == "77085202503540" then 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/tyxca01/ActriumHub/refs/heads/main/GrowaGarden2.lua"))()
 else 
     print("Unsupported PlaceId: "..placeIdStr)
